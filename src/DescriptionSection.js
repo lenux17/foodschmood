3cd5eea8546a7d054402a003773991ff319fd806
Olena <nuchuporuk@gmail.com>
2019-10-08 20:11:49 +0200

    add catalogue section

--- src/DescriptionSection.js
+++ src/DescriptionSection.js
@@ -4,14 +4,14 @@ import "./DescriptionSection.css";
 export const DescriptionSection = () => {
   return (
     <div className="DescriptionSection">
-      <h1>Products #####</h1>
+      <h1>Products Foodschmood</h1>
       <p>
-        The nutritional needs of cats change throughout life, which is why #####
-        offers delicious food, the recipe of which has been developed so that
-        your cat receives exactly what it needs at every stage of its life.
-        Thanks to the content of a specially selected composition of vitamins
-        and minerals, ##### foods help preserve your cat's health for many
-        years.
+        The nutritional needs of cats change throughout life, which is why
+        Foodschmood offers delicious food, the recipe of which has been
+        developed so that your cat receives exactly what it needs at every stage
+        of its life. Thanks to the content of a specially selected composition
+        of vitamins and minerals, Foodschmood foods help preserve your cat's
+        health for many years.
       </p>
     </div>
   );
