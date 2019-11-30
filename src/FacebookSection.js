7fc818facb695ec7e75f502aa1cacf49be82fe50
Olena <nuchuporuk@gmail.com>
2019-11-30 18:36:11 +0100

    change color and change pictures

--- src/FacebookSection.js
+++ src/FacebookSection.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import "./FacebookSection.css";
+import GreyFacebook from "./images/greyFacebook.png";
+import Facebook from "./images/facebook.png";
 
 export const FacebookSection = () => {
   return (
     <div className="facebookSection">
-      <img
-        className="f"
-        src="http://vellerupsommerby.dk/wp-content/uploads/2017/09/facebook-icon-logo-C61047A9E7-seeklogo.com_.png"
-      />
+      <img className="f" src={Facebook} />
       <h1>Cat's Community on Facebook</h1>
       <h3>
         Ask questions, brag about the photos and feats of your cat, win
@@ -15,10 +14,7 @@ export const FacebookSection = () => {
       </h3>
       <h2>Join cat fans</h2>
       <a href="#">
-        <img
-          className="link"
-          src="https://icon-library.net/images/facebook-icon-circle/facebook-icon-circle-24.jpg"
-        />
+        <img className="link" src={GreyFacebook} />
       </a>
     </div>
   );
